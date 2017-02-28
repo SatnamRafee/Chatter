2bdef0817db8cda8a5441967a12b6a5db4b58af6
Satnam Brar <satnamsbrar@gmail.com>
2017-02-28 15:08:03 -0500

    edited chatscontroller. edited chats index view

--- Chatter/Controllers/ChatsController.cs
+++ Chatter/Controllers/ChatsController.cs
@@ -17,10 +17,23 @@ namespace Chatter.Controllers
         // GET: Chats
         public ActionResult Index()
         {
-            return View(db.Chats.ToList());
-        }
+            Chat chat = db.Chats.Find();
 
-        // GET: Chats/Details/5
+            //Daniel:We can populate our new view model entity with a linq query. We have a lot of flexibility
+            var viewModel = new ChatViewModel
+            {
+                //We can reuse the comment form from a couple of lines above
+                Chat = chat,
+                //We don't want to pull back all the procedures, just the one with the same priority as our comment
+                ApplicationUser = (from p in db.Users
+                                   where p.ChatName == chat.ApplicationUser.ChatName
+                                   select p).First()
+            };
+            return View(viewModel);
+            //return View(db.Chats.ToList());
+
+            // GET: Chats/Details/5
+        }
         public ActionResult Details(int? id)
         {
             if (id == null)
