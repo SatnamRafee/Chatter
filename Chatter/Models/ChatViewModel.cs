7b565e329ea7a91fb077234ffd3a9be83bc901dd
Satnam Brar <satnamsbrar@gmail.com>
2017-02-28 14:28:38 -0500

    added chatviewmodel

---
+++ Chatter/Models/ChatViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Chatter.Models
+{
+    public class ChatViewModel
+    {
+        public ApplicationUser ApplicationUser { get; set; }
+        public Chat Chat { get; set; }
+    }
+}
